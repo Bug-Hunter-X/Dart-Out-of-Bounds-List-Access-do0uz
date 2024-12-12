6b1cf60552ce6df503dd2534a8f325f7b09e5206
BugHunter-X <askzenai@gmail.com>
2024-12-13 00:28:06 +0100

    Add bugSolution.dart file

---
+++ bugSolution.dart
@@ -0,0 +1,19 @@
+```dart
+List<int> numbers = [1, 2, 3, 4, 5];
+
+// Check if the index is within the bounds of the list before accessing it.
+if (numbers.length > 5 && 5 < numbers.length) {
+  int number = numbers[5];
+  print(number);
+} else {
+  print("Index out of bounds");
+}
+
+//Alternatively, use try-catch block:
+try {
+  int number = numbers[5];
+  print(number);
+} catch (e) {
+  print("Error: "+ e.toString());
+}
+```
